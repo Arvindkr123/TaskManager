9a4ff1c89ee2e746aa0d4ff051723113f3910e6b
Arvind Kumar <thakurarvindkr10@gmail.com>
2023-12-21 21:28:02 +0530

    created schema

--- controller/tasks.js
+++ controller/tasks.js
@@ -1,16 +1,26 @@
+const Task = require("../models/Tasks");
+
 const getAllTasks = (req, res) => {
   res.send("all items from the file");
 };
 
-const createTask = (req, res) => {
-  res.json(req.body);
+const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).send({ msg: error });
+  }
 };
+
 const getSignleTask = (req, res) => {
   res.send({ id: req.params.id });
 };
+
 const updateTask = (req, res) => {
   res.send("update task");
 };
+
 const deleteTask = (req, res) => {
   res.send("delete task");
 };
