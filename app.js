7ddcd73190101f3bc3b5394773780be7a1a0ab93
Arvind Kumar <thakurarvindkr10@gmail.com>
2023-12-21 20:35:56 +0530

    connected

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
+const connectDB = require("./db/connection");
+require("dotenv").config();
 
 // middleware
 app.use(express.json());
@@ -12,6 +14,16 @@ app.get("/hello", (req, res) => {
 // tasks routes
 app.use("/api/v1/tasks", tasks);
 
-app.listen(3000, () => {
-  console.log("server is listening on port 3000...");
-});
+const PORT = 3000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, () => {
+      console.log("server is listening on port 3000...");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
